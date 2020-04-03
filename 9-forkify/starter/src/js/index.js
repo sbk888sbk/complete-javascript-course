4333e005130f90abe35d74db930f81486ded9134
Sai Bala Krishna Allamsetty <sbk888.sbk@gmail.com>
2020-04-03 17:23:28 +0530

    .gitignore

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,7 +1,40 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView'
+import { elements } from './views/base';
+/** Global State of the app
+* - Search Object
+* - Current recipe object
+* - Liked recipes
+*/
+const state = {};
 
+const controlSearch = async () => {
+    //1. Get query from the view
+    const query = searchView.getInput();
+
+
+    if(query){
+        // 2. New Search object and add it to state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        
+        //4. Search for recipes
+        await state.search.getResults(search);
+
+        //5. Render results on UI
+        searchView.renderResults(state.search.result);
+        console.log(`state result is :`,state.search.result)
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
 
 const search = new Search('pizza');
 
-console.log(search);
-search.getResults(search)
+// console.log(search);
--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -8,10 +8,10 @@ export default class Search{
         try {
             const res = await axios(`${proxy}http://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
             this.result = res.data.recipes;
-            console.log(this.result)
+            // console.log(this.result)
         } catch(err){
             alert(err);
-            console.log(err);
+            // console.log(err);
         }
     }
 }
