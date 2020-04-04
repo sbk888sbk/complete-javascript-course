b3e5446604c8ade19b7b6d00e733dedc40742dd4
Sai Bala Krishna Allamsetty <sbk888.sbk@gmail.com>
2020-04-04 14:05:51 +0530

    Implemented Search Results Pagination

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,6 +1,8 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView'
 import { elements } from './views/base';
+import { renderLoader } from './views/base';
+import { clearLoader } from './views/base'
 /** Global State of the app
 * - Search Object
 * - Current recipe object
@@ -20,13 +22,15 @@ const controlSearch = async () => {
         // 3. Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
         
         //4. Search for recipes
         await state.search.getResults(search);
 
         //5. Render results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
-        console.log(`state result is :`,state.search.result)
+        // console.log(`state result is :`,state.search.result)
     }
 }
 
@@ -38,3 +42,14 @@ elements.searchForm.addEventListener('submit', e => {
 const search = new Search('pizza');
 
 // console.log(search);
+
+// Using event delegation concept
+elements.searchResPages.addEventListener('click', e=> {
+    const btn = e.target.closest('.btn-inline')
+    if(btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+        // console.log(goToPage);
+    }
+})
